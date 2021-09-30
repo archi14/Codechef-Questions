371fc6a6a6737e4b5e5e2e53c3fb8589fc887aa8
archi14 <archita810@gmail.com>
2021-09-30 17:16:27 +0530

    CookOffjan19

---
+++ CookOffJan19/adaschool.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,m;
+	    cin>>n>>m;
+	    if(n%2==0 || m%2==0)
+	    {
+	        cout<<"YES"<<endl;
+	    }else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+}
+
