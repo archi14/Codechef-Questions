3a2c5f9baf8edd90250ab91fd8059537171cbbc5
archi14 <archita810@gmail.com>
2022-05-05 13:12:17 +0530

    starter 37

---
+++ starter37-2022/minheight.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int x,h;
+        cin>>x>>h;
+        if(x>=h)
+        {
+            cout<<"YES"<<endl;
+        }else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+}
+
